e42655f4ad4c5b210fa7d6737e70266411dab27f
Baskaran Deivasigamani <baskaran.md@gmail.com>
2018-04-15 15:17:06 -0700

    [Stringers] Implement stringers for IPAddr type

---
+++ methods_18/README.md
@@ -0,0 +1,5 @@
+## Exercise: Stringers
+
+Make the `IPAddr` type implement `fmt.Stringer` to print the address as a dotted quad.
+
+For instance, `IPAddr{1, 2, 3, 4}` should print as `"1.2.3.4"`.
---
+++ methods_18/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+//IPAddr - Array of bytes
+type IPAddr [4]byte
+
+func (ip IPAddr) String() string {
+	var result, separator string
+	for _, octet := range ip {
+		result += separator + fmt.Sprintf("%v", octet)
+		separator = "."
+	}
+	return result
+}
+
+// Alternate method for printing
+/*
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+}
+*/
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
